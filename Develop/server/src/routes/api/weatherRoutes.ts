5503d40e568f9ef3d8c9c7d9dd6e61baeeabcd75
Tushar Patel <tusharpatel@Tushars-MacBook-Pro.local>
2024-09-20 17:21:06 -0400

    historyservice

--- Develop/server/src/routes/api/weatherRoutes.ts
+++ Develop/server/src/routes/api/weatherRoutes.ts
@@ -2,25 +2,32 @@ import { Router, type Request, type Response } from 'express';
 import weatherService from '../../service/weatherService';
 const router = Router();
 
-// import HistoryService from '../../service/historyService.js';
-// import WeatherService from '../../service/weatherService.js';
+import HistoryService from '../../service/historyService.js';
+
 
 // TODO: POST Request with city name to retrieve weather data
 router.post('/', (req: Request, res: Response) => {
   // TODO: GET weather data from city name
+  weatherService.getWeatherForCity(req.body.cityName).then((weatherData) => {
+    HistoryService.addCity(req.body.cityName);
 
-  
+    res.json(weatherData);
+  });
 
-  
-  
-  
   // TODO: save city to search history
 });
 
 // TODO: GET search history
-router.get('/history', async (req: Request, res: Response) => {});
+router.get('/history', async (req: Request, res: Response) => { 
+  HistoryService.getCities().then((cities) => {
+    res.json(cities);
+  });
+});
 
 // * BONUS TODO: DELETE city from search history
-router.delete('/history/:id', async (req: Request, res: Response) => {});
+router.delete('/history/:id', async (req: Request, res: Response) => { 
+  HistoryService.removeCity(req.params.id);
+  res.sendStatus(200);
+});
 
 export default router;
--- Develop/server/src/service/historyService.ts
+++ Develop/server/src/service/historyService.ts
@@ -37,7 +37,11 @@ class HistoryService {
     await this.write(cities);
   }
   // * BONUS TODO: Define a removeCity method that removes a city from the searchHistory.json file
-  // async removeCity(id: string) {}
+  async removeCity(id: string) {
+    const cities = await this.read();
+    const updatedCities = cities.filter((city: City) => city.id !== id);
+    await this.write(updatedCities);
+  }
 }
 
 export default new HistoryService();
